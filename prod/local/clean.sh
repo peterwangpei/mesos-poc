682c29d8eb1906a0b63bdc52925913ae812312d2
zhpooer <zhpooer@gmail.com>
2015-12-23 09:32:16 +0800

    add vagrant micro surpport

--- prod/local/clean.sh
+++ prod/local/clean.sh
@@ -1,7 +1,8 @@
-kubectl delete rc --all --server=http://192.168.33.41:8888
-kubectl delete svc --all --server=http://192.168.33.41:8888
-kubectl delete pv --all --server=http://192.168.33.41:8888
-kubectl delete pvc --all --server=http://192.168.33.41:8888
-kubectl delete ingress --all --server=http://192.168.33.41:8888
-kubectl delete rc --namespace=kube-system --all --server=http://192.168.33.41:8888
-kubectl delete svc --namespace=kube-system --all --server=http://192.168.33.41:8888
+kubectl delete rc --all --server=http://192.168.33.11
+kubectl delete svc --all --server=http://192.168.33.11
+kubectl delete pv --all --server=http://192.168.33.11
+kubectl delete pvc --all --server=http://192.168.33.11
+kubectl delete ingress --all --server=http://192.168.33.11
+kubectl delete rc --namespace=kube-system --all --server=http://192.168.33.11
+kubectl delete svc --namespace=kube-system --all --server=http://192.168.33.11
+kubectl delete secret --all --server=http://192.168.33.11
