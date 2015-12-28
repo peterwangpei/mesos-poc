c3e8172de1c0f1a95fa4a19e87f18732001f5ca8
qinghua <qhgao@thoughtworks.com>
2015-12-28 10:41:18 +0800

    [qhgao] use ceph for mysql replication

--- docker/mysql-slave/my_entrypoint.sh
+++ docker/mysql-slave/my_entrypoint.sh
@@ -4,7 +4,7 @@ set -e -m
 echo "start configurating mysql database"
 source /entrypoint.sh &
 sleep 20s
-mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "drop database mysql; CHANGE MASTER TO MASTER_HOST='${MYSQL_MASTER_SERVICE_HOST}', MASTER_USER='peter', MASTER_PASSWORD='123456'; start slave;"
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "drop database mysql; CHANGE MASTER TO MASTER_HOST='${MYSQL_MASTER_SERVICE_HOST}', MASTER_USER='peter', MASTER_PASSWORD='123456'; start slave;"
 
 echo "end configurating mysql database"
 fg
