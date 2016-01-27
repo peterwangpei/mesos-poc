bbdba62679327393d648ef365676f87d70023eca
zengezenge <zengezenge@gmail.com>
2016-01-27 15:31:16 +0800

    [ZE] Fixed some bugs

--- performance/loadbalancer.py
+++ performance/loadbalancer.py
@@ -1,152 +1,212 @@
 #!/usr/bin/env python
 
-import urllib2
-import sys
 import json
-import requests
+import sys
+import urllib2
+import subprocess
 import threading
-import os
-from urllib import urlencode
-from urllib import quote
-from mako.template import Template
+from Queue import Queue
+from Queue import Empty
 from optparse import OptionParser
+import datetime
+import logging
 from urlgrabber.keepalive import HTTPHandler
 
-server = None
-templatefile = None
-configfile = None
 
-class EndPoint:
-    address = ""
-    port = ""
-
-    def __init__(self,address, port):
-        self.address = address
-        self.port = port
-
-class Service:
-    name = ""
-    endpoints = []
-
-    def __init__(self, name, endpoints):
-        self.name = name
-        self.endpoints = endpoints
-
-def generateconfig(server,configfile):
-    services = loadservices(server)
-    template = Template(filename=templatefile)
-    content =  template.render(items=services)
-
+def loadTestCase(filename):
     try:
-        file = open(configfile,"w")
-        file.write(content)
+        fp = open(filename, "r")
+        return json.load(fp)
     finally:
-        file.close()
+        fp.close()
 
-def reloadbalancer():
-    generateconfig(server,configfile)
-    os.system("/bin/bash reload.sh {0}".format(configfile))
 
-def loadjson(url):
-    try:
-        request = requests.get(url)
-        return json.loads(request.content)
-    finally:
-        request.close()
+def execCommandWithCheck(command):
+    subprocess.check_call(command, shell=True)
+    print command
 
-def loadservices(server):
 
-    serviceurl = "http://{0}/api/v1/namespaces/default/services".format(server)
+def watchEvents(case, queue):
+    watchurl = "http://{0}/api/v1/namespaces/{1}/pods?watch=true".format(case["api_server"], case["namespace"])
+    stream = urllib2.urlopen(watchurl)
 
-    jsonservices = loadjson(serviceurl)
+    event = ""
+    length = -1
 
-    services = []
+    while True:
 
-    for s in jsonservices["items"]:
+        if event.endswith("\r\n"):
+            event = event.rstrip()
 
-        if not "name" in s["metadata"]:
+            if len(event) > 0:
+                length = int(event, 16)
+                event = ""
+
+        if length == -1:
+            event += stream.read(1)
+        else:
+            event = stream.read(length)
+            queue.put_nowait(event)
+            length = -1
+            event = ""
+
+
+def execCommand(command):
+    subprocess.call(command, shell=True)
+    print command
+
+
+def outputRecord(record, target):
+    print "%s---%s pods left" % (datetime.datetime.now(), target)
+
+
+def processEvents(queue, target, beginTime, checker, ):
+    endTime = datetime.datetime.min
+    while True:
+        try:
+            event = queue.get_nowait()
+
+            record = eval(checker)(event, beginTime, )
+            if record:
+                target -= 1
+                outputRecord(record, target)
+
+                if record[3] > endTime:
+                    endTime = record[3];
+
+            if target == 0:
+                return endTime
+
+            if target < 0:
+                raise Exception("Invalid target condition")
+
+            queue.task_done()
+        except Empty:
             continue
 
-        name = quote(s["metadata"]["name"])
 
-        endpointurl = "http://{0}/api/v1/namespaces/default/endpoints/{1}".format(server,name)
+def checkDeletionEvent(event, beginTime, ):
+    data = json.loads(event)
 
-        jsonendpoint = loadjson(endpointurl)
+    object = data["object"]
 
-        endpoints = []
+    if data["type"] != "DELETED":
+        return
 
-        for subset in jsonendpoint["subsets"]:
-            for address in subset["addresses"]:
-                for port in subset["ports"]:
-                    endpoint = EndPoint(address["ip"],port["port"])
-                    endpoints.append(endpoint)
+    if not object.has_key("status"):
+        return
 
-        service = Service(s["metadata"]["name"],endpoints)
+    if not object["status"].has_key("containerStatuses"):
+        return
 
-        services.append(service)
+    metadata = object["metadata"]
+    status = object["status"]
+    containerStatuses = status["containerStatuses"]
+    deletionTime = datetime.datetime.min
 
-    return services
+    for container in containerStatuses:
+        if not container["state"].has_key("terminated"):
+            return
+
+        containerDelectionTime = datetime.datetime.strptime(metadata["deletionTimestamp"], "%Y-%m-%dT%H:%M:%SZ")
+        if containerDelectionTime > deletionTime:
+            deletionTime = containerDelectionTime
+
+    logging.debug("%s,%s,%s,%f", "MATCH", beginTime.strftime("%Y/%m/%d %H:%M:%S.%f"),
+                  deletionTime.strftime("%Y/%m/%d %H:%M:%S.%f"), (deletionTime - beginTime).total_seconds())
+
+    return True, data, beginTime, deletionTime,
+
+
+def checkCreationEvent(event, beginTime, ):
+    data = json.loads(event)
+
+    object = data["object"]
+
+    if data["type"] != "MODIFIED":
+        return
+
+    if not object.has_key("status"):
+        return
+
+    if not object["status"].has_key("containerStatuses"):
+        return
+
+    status = object["status"]
+    containerStatuses = status["containerStatuses"]
+    startedTime = datetime.datetime.min
+
+    for container in containerStatuses:
+        if not container["state"].has_key("running"):
+            return
+
+        if not container["ready"]:
+            return
+
+        containerStartedTime = datetime.datetime.strptime(container["state"]["running"]["startedAt"],
+                                                          "%Y-%m-%dT%H:%M:%SZ")
+        if containerStartedTime > startedTime:
+            startedTime = containerStartedTime
+
+    startTime = datetime.datetime.strptime(status["startTime"], "%Y-%m-%dT%H:%M:%SZ")
+
+    logging.debug("%s,%s,%s,%f", "MATCH", startTime.strftime("%Y/%m/%d %H:%M:%S.%f"),
+                  startedTime.strftime("%Y/%m/%d %H:%M:%S.%f"), (startedTime - startTime).total_seconds())
+
+    return True, data, startTime, startedTime,
+
 
 def main(argv):
+    global case
+    global isachieved
+    global exit_condition
+    global queue
+    global lasttime
 
-    global server
-    global templatefile
-    global configfile
+    queue = Queue();
 
-    parser = OptionParser(usage="%prog -s url")
-    parser.add_option("-s","--server", dest="server", help="The address and port of the Kubernetes API server")
-    parser.add_option("-t","--template", dest="template", help="The template of haproxy config file", default="template.cfg")
-    parser.add_option("-c","--config", dest="config", help="The config file path of haproxy", default="/etc/haproxy/haproxy.cfg")
+    parser = OptionParser(usage="%prog -c case")
+    parser.add_option("-c", "--case", dest="case", help="The json config file path of test case")
 
     (options, argv) = parser.parse_args(argv)
 
-    print options.template
-
-    if options.server is None:
-        parser.error("The address and port of the Kubernetes API server was not given")
-
-    if options.template is None:
-        parser.error("The template of haproxy config file was not given")
-
-    if options.config is None:
-        parser.error("The config file path of haproxy was not given")
+    if options.case is None:
+        parser.error("The yaml config file path of test case was not given")
 
     handler = HTTPHandler()
     opener = urllib2.build_opener(handler)
     urllib2.install_opener(opener)
 
-    server = options.server
-    templatefile = options.template
-    configfile = options.config
+    case = loadTestCase(options.case)
 
-    serviceurl = "http://{0}/api/v1/namespaces/default/endpoints?watch=true".format(options.server)
-    stream = urllib2.urlopen(serviceurl)
+    target = int(case["exit_condition"])
+    checker = case["checker"]
 
-    message = ""
-    length = -1
-    reloadtimer = None
+    logging.basicConfig(level=logging.NOTSET,
+                        format='%(message)s',
+                        filename="{0}_{1}.log".format(case["log_name"],
+                                                      datetime.datetime.now().strftime("%Y%m%d%H%M%S")),
+                        filemode='w')
 
-    while True:
+    try:
+        beginTime = datetime.datetime.now()
+        logging.debug("%s,%s,%s,%s", "START", beginTime.strftime("%Y/%m/%d %H:%M:%S.%f"), "", "")
 
-        if message.endswith("\r\n"):
-            message = message.rstrip()
+        execCommandWithCheck(case["start_command"])
 
-            if len(message) > 0:
-                length = int(message,16)
-                message = ""
+        thread = threading.Thread(target=watchEvents, args=(case, queue,))
+        thread.daemon = True
+        thread.start()
 
-        if length == -1:
-            message += stream.read(1)
-        else:
-            message = stream.read(length)
-            length = -1
-            message = ""
+        endTime = processEvents(queue, target, beginTime, checker, )
 
-            if reloadtimer is not None:
-                reloadtimer.cancel()
+        logging.debug("%s,%s,%s,%f", "STOP", beginTime.strftime("%Y/%m/%d %H:%M:%S.%f"),
+                      endTime.strftime("%Y/%m/%d %H:%M:%S.%f"), (endTime - beginTime).total_seconds())
 
-            reloadtimer = threading.Timer(0.5, reloadbalancer)
-            reloadtimer.start()
+    finally:
+        if case.has_key("clear_command") and case["clear_command"]:
+            execCommand(case["clear_command"])
+
 
 if __name__ == "__main__":
     main(sys.argv)
