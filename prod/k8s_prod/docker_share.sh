30248d1d42e9257eff62f60f085e531e5f624b45
zhpooer <zhpooer@gmail.com>
2016-02-26 16:50:24 +0800

    refactor add new registry

---
+++ prod/k8s_prod/docker_share.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+set -e
+
+TAG_WITH_DEST=192.168.33.110:5000/$1
+
+if [ -nz $2 ]; then
+    docker pull $1
+fi
+
+docker tag -f $1 $TAG_WITH_DEST
+docker push $TAG_WITH_DEST
+docker rmi $TAG_WITH_DEST
